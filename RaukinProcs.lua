b8c5d0e11085b5a8968bbd7172ffc7e197b25015
Lars de Lange <lars.delange@ampelmann.nl>
2020-08-04 10:38:42 +0200

    Update RaukinProcs.lua

--- RaukinProcs.lua
+++ RaukinProcs.lua
@@ -22,37 +22,43 @@ function RaukinProcs.ADDON_LOADED(self,event,arg1)
 		RaukinProcsDB.alpha = RaukinProcsDB.alpha or 1
 		RaukinProcsDB.point = RaukinProcsDB.point or "CENTER"
 
-------------------------------------------- Frame -------------------------------------------
-		Rframe = CreateFrame("Frame",nil,UIParent)
-		Rframe:SetFrameStrata("HIGH")
-		Rframe:SetWidth(RaukinProcsDB.width)
-		Rframe:SetHeight(RaukinProcsDB.height)
-		Rframe:SetAlpha(RaukinProcsDB.alpha)
+		RaukinProcsDB.barvis = RaukinProcsDB.barvis or true
+		RaukinProcsDB.baramount = RaukinProcsDB.baramount or 5
+		RaukinProcsDB.bartexture = RaukinProcsDB.bartexture or "Aluminium"
+		RaukinProcsDB.barfont = RaukinProcsDB.barfont or "Arial Narrow"
+		RaukinProcsDB.barfontsize = RaukinProcsDB.barfontsize or 14
+		
 
-		tR = Rframe:CreateTexture(nil,"BACKGROUND")
+------------------------------------------- Iconframe1 -------------------------------------------
+		IconFrame1 = CreateFrame("Frame",nil,UIParent)
+		IconFrame1:SetFrameStrata("HIGH")
+		IconFrame1:SetWidth(RaukinProcsDB.width)
+		IconFrame1:SetHeight(RaukinProcsDB.height)
+		IconFrame1:SetAlpha(RaukinProcsDB.alpha)
+
+		tR = IconFrame1:CreateTexture(nil,"BACKGROUND")
 		_,_,dsIcon = GetSpellInfo(408) 
 		tR:SetTexture(dsIcon)
 		tR:SetTexCoord(.07, .93, .07, .93)
-		tR:SetAllPoints(Rframe)
-		Rframe.texture = tR
+		tR:SetAllPoints(IconFrame1)
+		IconFrame1.texture = tR
 
-		tRcolor = Rframe:CreateTexture(nil,"LOW")
-		tRcolor:SetAllPoints(Rframe)
-		tRcolor:SetTexture(0,1,0,1)
-		Rframe.texture = tRcolor
-		Rframe.texture:SetAlpha(0)
-
-		Rframe:SetPoint(RaukinProcsDB.point,RaukinProcsDB.posX,RaukinProcsDB.posY)
-		Rframe:RegisterForDrag("LeftButton")
-		Rframe:SetScript("OnDragStart", Rframe.StartMoving)
-		Rframe:SetScript("OnDragStop", 
+		IconFrame1:SetPoint(RaukinProcsDB.point,RaukinProcsDB.posX,RaukinProcsDB.posY)
+		IconFrame1:RegisterForDrag("LeftButton")
+		IconFrame1:SetScript("OnDragStart", IconFrame1.StartMoving)
+		IconFrame1:SetScript("OnDragStop", 
 			function() 
-				Rframe:StopMovingOrSizing() 
-				point, relativeTo, relativePoint, xOfs, yOfs = Rframe:GetPoint()
+				IconFrame1:StopMovingOrSizing() 
+				point, relativeTo, relativePoint, xOfs, yOfs = IconFrame1:GetPoint()
 				RaukinProcsDB.point = point
 				RaukinProcsDB.posX = xOfs 
 				RaukinProcsDB.posY = yOfs 
 			end)
+		
+------------------------------------------- BarFrame1 -------------------------------------------		
+		BarFrame1 = CreateFrame("Frame",nil,UIParent)
+
+
 		RaukinProcs.MakeOptions()
 
 	end
@@ -70,7 +76,31 @@ function RaukinProcs.ChangeBackground(f,Frame, Icon)
 	Frame.texture = f
 end
 
+function RaukinProcs.UpdateFrame()
+	IconFrame1:SetWidth(RaukinProcsDB.width)
+	IconFrame1:SetHeight(RaukinProcsDB.height)
+	IconFrame1:SetAlpha(RaukinProcsDB.alpha)
+	
+end
+
 function RaukinProcs.MakeOptions(self)
+    for i,v in pairs(media:List('statusbar')) do
+        bars[v] = v
+    end
+    media.RegisterCallback(RaukinProcs, "LibSharedMedia_Registered",
+        function(event, mediatype, key)
+            if mediatype == 'font' then
+                fonts[key] = key
+                if key == RaukinProcsDB.barfont then
+                    BarFrame1.text:SetFont(media:Fetch('font',RaukinProcsDB.barfont),RaukinProcsDB.barfontsize)
+                end
+            elseif mediatype == 'statusbar' then
+                bars[key] = key
+                if key == RaukinProcsDB.bartexture then
+                    if RaukinProcsDB.barvis then BarFrame1:SetTexture(media:Fetch('statusbar',RaukinProcsDB.bartexture)) end
+                end
+            end
+        end)
 
     local opt = {
 		type = 'group',
@@ -89,6 +119,7 @@ function RaukinProcs.MakeOptions(self)
                 order = 1,
                 args = {
                     Drop = {
+			order = 1,
                         name = "Spell Id Select",
                         type = "select",
 			values = {112, 113, 114},
@@ -96,6 +127,12 @@ function RaukinProcs.MakeOptions(self)
                         get = function(info) end,
                         set = function(info, s) end,
                     },
+                    DeleteButton = {
+			order = 2,
+                        name = "Delete",
+                        type = "execute",
+			func = function(info) end,
+		    },
                 },
             }, 
             InputBox1 = {
@@ -105,10 +142,17 @@ function RaukinProcs.MakeOptions(self)
                 order = 2,
                 args = {
                     SpellIdInput = {
+			order = 1,
                         name = "SpellId",
                         type = "input",
                         get = function(info) end,
                         set = function(info, s) end,
+                    },
+                    InputButton = {
+			order = 2,
+                        name = "Input",
+                        type = "execute",
+			func = function(info) end,
                     },
                 },
             }, 
@@ -119,34 +163,15 @@ function RaukinProcs.MakeOptions(self)
                 order = 3,
                 args = {
                     MCDInput = {
+			order = 1,
                         name = "Cooldown",
                         type = "input",
                         get = function(info) end,
                         set = function(info, s) end,
                     },
-                },
-            }, 
-            InputButton = {
-                type = "group",
-                name = "Input",
-                guiInline = true,
-                order = 4,
-                args = {
-                    InputButton = {
-                        name = "Input",
-                        type = "execute",
-			func = function(info) end,
-                    },
-                },
-            },
-            DeleteButton = {
-                type = "group",
-                name = "Delete",
-                guiInline = true,
-                order = 5,
-                args = {
-                    DeleteButton = {
-                        name = "Delete",
+                    UpdateButton = {
+			order = 2,
+                        name = "Update",
                         type = "execute",
 			func = function(info) end,
                     },
@@ -161,7 +186,19 @@ function RaukinProcs.MakeOptions(self)
                     Button = {
                         name = "Move Bars",
                         type = "execute",
-			func = function(info) end,
+			func = function(info) 
+				if RaukinProcsDB.Moveable then
+					IconFrame1:EnableMouse(false)
+					IconFrame1:SetMovable(false)
+					RaukinProcsDB.Moveable=false
+					RaukinProcs.ChangeBackground(tR,IconFrame1, 14176)	
+				else
+					IconFrame1:EnableMouse(true)
+					IconFrame1:SetMovable(true)
+					RaukinProcsDB.Moveable=true
+					RaukinProcs.ChangeBackground(tR,IconFrame1, 25820)	
+				end;
+			end,
                     },
                 },
             },            
@@ -175,7 +212,7 @@ function RaukinProcs.MakeOptions(self)
                         name = "Target Icon Size",
                         type = "range",
                         get = function(info) return RaukinProcsDB.width end,
-                        set = function(info, s) RaukinProcsDB.width = s; RaukinProcsDB.height = s; end,
+                        set = function(info, s) RaukinProcsDB.width = s; RaukinProcsDB.height = s; RaukinProcs.UpdateFrame(); end,
                         min = 1,
                         max = 400,
                         step = 1,
@@ -199,9 +236,9 @@ function RaukinProcs.MakeOptions(self)
                     },
                 },
             },
-            BarTog = {
+            Options = {
                 type = "group",
-                name = "Only Icons",
+                name = "Options",
                 guiInline = true,
                 order = 9,
                 args = {
@@ -210,6 +247,15 @@ function RaukinProcs.MakeOptions(self)
                         type = "toggle",
                         get = function(info) end,
                         set = function(info, s) end,
+                    },
+                    AmountBars = {
+                        name = "Max amount of procs",
+                        type = "range",
+                        get = function(info) end,
+                        set = function(info, s) end,
+			min = 1,
+			max = 10,
+			step = 1,
                     },
                 },
             }, 
